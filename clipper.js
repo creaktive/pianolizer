5a78bc6a72f8ef781406486e4fe95b4560df3605
Stanislaw Pusep <creaktive@gmail.com>
2021-09-19 14:44:41 +0200

    cleanup

--- clipper.js
+++ clipper.js
@@ -1,17 +1,19 @@
 class SoftClipper extends AudioWorkletProcessor {
-  constructor() {
-    super()
-  }
-  static get parameterDescriptors() {
+  // constructor () {
+  //   super()
+  // }
+
+  static get parameterDescriptors () {
     return [{
       name: 'amount',
       defaultValue: 0.5,
       minValue: 0,
       maxValue: 10,
-      automationRate: "k-rate"
-    }];
+      automationRate: 'k-rate'
+    }]
   }
-  process(input, output, parameters) {
+
+  process (input, output, parameters) {
     // `input` is an array of input ports, each having multiple channels.
     // For each channel of each input port, a Float32Array holds the audio
     // input data.
@@ -20,20 +22,20 @@ class SoftClipper extends AudioWorkletProcessor {
     // to output data.
     // `parameters` is an object having a property for each parameter
     // describing its value over time.
-    let amount = parameters["amount"][0];
-    let inputPortCount = input.length;
+    const amount = parameters.amount[0]
+    // const inputPortCount = input.length
     for (let portIndex = 0; portIndex < input.length; portIndex++) {
-      let channelCount = input[portIndex].length;
+      const channelCount = input[portIndex].length
       for (let channelIndex = 0; channelIndex < channelCount; channelIndex++) {
-        let sampleCount = input[portIndex][channelIndex].length;
+        const sampleCount = input[portIndex][channelIndex].length
         for (let sampleIndex = 0; sampleIndex < sampleCount; sampleIndex++) {
           output[0][channelIndex][sampleIndex] =
-            Math.tanh(amount * input[portIndex][channelIndex][sampleIndex]);
+            Math.tanh(amount * input[portIndex][channelIndex][sampleIndex])
         }
       }
     }
-    return true;
+    return true
   }
 }
 
-registerProcessor('soft-clipper-node', SoftClipper);
+registerProcessor('soft-clipper-node', SoftClipper)
