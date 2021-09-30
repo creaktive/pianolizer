bef784341b50f09f6e977db0075734870ec2c080
Stanislaw Pusep <creaktive@gmail.com>
2021-09-30 19:22:47 +0200

    61 keys!

--- piano.js
+++ piano.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 class PianoOctave {
   // Shamelessly stolen from http://www.quadibloc.com/other/cnv05.htm
-  constructor (scale = 1.0) {
+  constructor (scale = 1) {
     this.scale = scale
 
     this.whiteHeight = 150 * scale
@@ -14,8 +14,9 @@ class PianoOctave {
     this.ns = 'http://www.w3.org/2000/svg'
   }
 
-  drawKey (offset, width, height, group) {
+  drawKey (index, offset, width, height, group) {
     const keyElement = document.createElementNS(this.ns, 'rect')
+    keyElement.setAttribute('data-ref', `pianoKey${index}`)
     keyElement.setAttribute('x', offset)
     keyElement.setAttribute('y', 0)
     keyElement.setAttribute('rx', 2)
@@ -27,29 +28,29 @@ class PianoOctave {
 
   // Inspired by https://github.com/davidgilbertson/sight-reader/blob/master/app/client/Piano.js
   drawKeyboard (svgId) {
-    // render whites
     const whiteKeyGroup = document.createElementNS(this.ns, 'g')
-    for (
-      let i = 0, offset = 0;
-      i < this.whiteKeys.length;
-      offset += this.whiteKeys[i++]
-    ) {
-      // const tone = this.whiteTone[i]
-      this.drawKey(offset, this.whiteKeys[i], this.whiteHeight, whiteKeyGroup)
-    }
-    svgId.appendChild(whiteKeyGroup)
-
-    // render blacks
     const blackKeyGroup = document.createElementNS(this.ns, 'g')
-    for (
-      let i = 0, offset = 0;
-      i < this.blackKeys.length;
-      offset += this.blackKeys[i++]
-    ) {
-      const tone = this.blackTone[i]
-      if (tone === 0) { continue }
-      this.drawKey(offset, this.blackKeys[i], this.blackHeight, blackKeyGroup)
+
+    let blackOffset = 0
+    let whiteOffset = 0
+    let whiteIndex = 0
+    for (let i = 0; i < 61; i++) {
+      // black
+      const blackIndex = i % this.blackKeys.length
+      const blackWidth = this.blackKeys[blackIndex]
+      if (this.blackTone[blackIndex]) {
+        this.drawKey(i, blackOffset, blackWidth, this.blackHeight, blackKeyGroup)
+      } else {
+        // white
+        const whiteWidth = this.whiteKeys[whiteIndex % this.whiteKeys.length]
+        this.drawKey(i, whiteOffset, whiteWidth, this.whiteHeight, whiteKeyGroup)
+        whiteIndex++
+        whiteOffset += whiteWidth
+      }
+      blackOffset += blackWidth
     }
+
+    svgId.appendChild(whiteKeyGroup)
     svgId.appendChild(blackKeyGroup)
   }
 }
