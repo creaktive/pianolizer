861e01b8e4cdab7f6d5a7379fd55c8470badeaf8
Stanislaw Pusep <creaktive@gmail.com>
2021-09-30 18:28:11 +0200

    refactor with drawKey()

--- piano.js
+++ piano.js
@@ -11,35 +11,36 @@ class PianoOctave {
     this.blackKeys = [14, 14, 14, 14, 14, 13, 14, 13, 14, 13, 14, 13].map(x => x * scale)
     this.blackTone = [0, 2, 0, 4, 0, 0, 7, 0, 9, 0, 11, 0]
 
-    this.width = this.whiteKeys.reduce((a, b) => a + b)
+    this.ns = 'http://www.w3.org/2000/svg'
+  }
+
+  drawKey (offset, width, height, group) {
+    const keyElement = document.createElementNS(this.ns, 'rect')
+    keyElement.setAttribute('x', offset)
+    keyElement.setAttribute('y', 0)
+    keyElement.setAttribute('rx', 2)
+    keyElement.setAttribute('width', width)
+    keyElement.setAttribute('height', height)
+    keyElement.setAttribute('style', 'stroke: #000; fill: #fff')
+    group.appendChild(keyElement)
   }
 
   // Inspired by https://github.com/davidgilbertson/sight-reader/blob/master/app/client/Piano.js
-  draw (svgId) {
-    const ns = 'http://www.w3.org/2000/svg'
-
+  drawKeyboard (svgId) {
     // render whites
-    const whiteKeyGroup = document.createElementNS(ns, 'g')
+    const whiteKeyGroup = document.createElementNS(this.ns, 'g')
     for (
       let i = 0, offset = 0;
       i < this.whiteKeys.length;
       offset += this.whiteKeys[i++]
     ) {
       // const tone = this.whiteTone[i]
-
-      const keyElement = document.createElementNS(ns, 'rect')
-      keyElement.setAttribute('x', offset)
-      keyElement.setAttribute('y', 0)
-      keyElement.setAttribute('rx', 2)
-      keyElement.setAttribute('width', this.whiteKeys[i])
-      keyElement.setAttribute('height', this.whiteHeight)
-      keyElement.setAttribute('style', 'stroke: #000; fill: #fff')
-      whiteKeyGroup.appendChild(keyElement)
+      this.drawKey(offset, this.whiteKeys[i], this.whiteHeight, whiteKeyGroup)
     }
     svgId.appendChild(whiteKeyGroup)
 
     // render blacks
-    const blackKeyGroup = document.createElementNS(ns, 'g')
+    const blackKeyGroup = document.createElementNS(this.ns, 'g')
     for (
       let i = 0, offset = 0;
       i < this.blackKeys.length;
@@ -47,15 +48,7 @@ class PianoOctave {
     ) {
       const tone = this.blackTone[i]
       if (tone === 0) { continue }
-
-      const keyElement = document.createElementNS(ns, 'rect')
-      keyElement.setAttribute('x', offset)
-      keyElement.setAttribute('y', 0)
-      keyElement.setAttribute('rx', 2)
-      keyElement.setAttribute('width', this.blackKeys[i])
-      keyElement.setAttribute('height', this.blackHeight)
-      keyElement.setAttribute('style', 'stroke: #000; fill: #fff')
-      blackKeyGroup.appendChild(keyElement)
+      this.drawKey(offset, this.blackKeys[i], this.blackHeight, blackKeyGroup)
     }
     svgId.appendChild(blackKeyGroup)
   }
