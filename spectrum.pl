b9b9a7b99988ae4b7047d4d2ef63032ffaa027f4
Stanislaw Pusep <creaktive@gmail.com>
2021-10-03 17:23:47 +0200

    simplify

--- spectrum.pl
+++ spectrum.pl
@@ -4,70 +4,50 @@ use experimental qw( signatures );
 use warnings qw( all );
 no warnings qw( experimental::signatures );
 
-use Carp ();
+use Cpanel::JSON::XS ();
 use POSIX ();
 
-# Ported to Perl from: https://github.com/ArashPartow/bitmap/blob/master/bitmap_image.hpp
-# Credits: Dan Bruton http://www.physics.sfasu.edu/astro/color.html
-sub convert_wave_length_nm_to_rgb ($wave_length_nm, $gamma = 0.8) {
-    my ( $red, $green, $blue );
+# use constant PI => 3.1415926535897932384626433832795;
+#
+# sub rainbow ($p) {
+#     return [
+#         POSIX::floor( 128 + 127 * sin( PI + $p ) ),
+#         POSIX::floor( 128 + 127 * sin( PI + $p + 2 * PI / 3 ) ),
+#         POSIX::floor( 128 + 127 * sin( PI + $p + 4 * PI / 3 ) ),
+#     ];
+# }
+#
+# my @colors;
+# for (my $i = 0; $i < 2 * PI; $i += ( PI / 6 ) ) {
+#    unshift @colors => rainbow( $i );
+# }
+# push @colors => shift @colors;
+# push @colors => shift @colors;
 
-    if ( ( 380 <= $wave_length_nm ) && ( $wave_length_nm < 440 ) ) {
-        $red    = -( $wave_length_nm - 440 ) / ( 440 - 380 );
-        $green  = 0;
-        $blue   = 1;
-    } elsif ( ( 440 <= $wave_length_nm ) && ( $wave_length_nm < 490 ) ) {
-        $red    = 0;
-        $green  = ( $wave_length_nm - 440 ) / ( 490 - 440 );
-        $blue   = 1;
-    } elsif ( ( 490 <= $wave_length_nm ) && ( $wave_length_nm < 510 ) ) {
-        $red    = 0;
-        $green  = 1;
-        $blue   = -( $wave_length_nm - 510 ) / ( 510 - 490 );
-    } elsif ( ( 510 <= $wave_length_nm ) && ( $wave_length_nm < 580 ) ) {
-        $red    = ($wave_length_nm - 510) / (580 - 510);
-        $green  = 1;
-        $blue   = 0;
-    } elsif ( ( 580 <= $wave_length_nm ) && ( $wave_length_nm < 645 ) ) {
-        $red    = 1;
-        $green  = -( $wave_length_nm - 645 ) / ( 645 - 580 );
-        $blue   = 0;
-    } elsif ( ( 645 <= $wave_length_nm ) && ( $wave_length_nm <= 780 ) ) {
-        $red    = 1;
-        $green  = 0;
-        $blue   = 0;
-    } else {
-        Carp::carp("Wave length of $wave_length_nm nm is outside of the visible range (380-780 nm)");
-        $red = $green = $blue = 0;
-    }
+my @colors = (
+    [0xff, 0x00, 0x00], # red
+    [0xff, 0x80, 0x00], # orange
+    [0xff, 0xff, 0x00], # yellow
+    [0x80, 0xff, 0x00], # chartreuse
+    [0x00, 0xff, 0x00], # green
+    [0x00, 0xff, 0x80], # spring
+    [0x00, 0xff, 0xff], # cyan
+    [0x00, 0x80, 0xff], # azure
+    [0x00, 0x00, 0xff], # blue
+    [0x80, 0x00, 0xff], # violet
+    [0xff, 0x00, 0xff], # magenta
+    [0xff, 0x00, 0x80], # rose
+);
 
-    my $factor;
-
-    if ( ( 380 <= $wave_length_nm ) && ( $wave_length_nm < 420 ) ) {
-        $factor = 0.3 + 0.7 * ( $wave_length_nm - 380 ) / ( 420 - 380 );
-    } elsif ( ( 420 <= $wave_length_nm ) && ( $wave_length_nm < 700 ) ) {
-        $factor = 1;
-    } elsif ( ( 700 <= $wave_length_nm ) && ( $wave_length_nm <= 780 ) ) {
-        $factor = 0.3 + 0.7 * ( 780 - $wave_length_nm ) / ( 780 - 700 );
-    } else {
-        $factor = 0;
-    }
-
-    return map {
-        ( $_ * $factor ) ** $gamma
-    } ( $red, $green, $blue );
-}
+say Cpanel::JSON::XS::encode_json( \@colors );
+exit;
 
 my $scale = 64;
-my @wavelengths = map { 299792.458 / (440 * (2 ** ($_ / 12))) } -2 .. 9;
 my @rainbow;
-for my $nm (@wavelengths) {
+for my $c ( @colors ) {
     for ( 1 .. $scale ) {
         push @rainbow =>
-            sprintf '%3d %3d %3d',
-                map {
-                    POSIX::round( $_ * 255 )
-                } convert_wave_length_nm_to_rgb( $nm, 0.5 );
+            sprintf '%3d %3d %3d', @$c
     }
 }
 
