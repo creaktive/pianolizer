f804fde1207a64f09373371d57f8ce7f3f1086d1
Stanislaw Pusep <creaktive@gmail.com>
2021-10-04 13:16:15 +0200

    n/K calculator

---
+++ bins.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+use 5.020;
+use experimental qw( signatures );
+use warnings qw( all );
+no warnings qw( experimental::signatures );
+
+use POSIX ();
+
+sub key2freq ( $n ) {
+    return 440 * ( 2 ** ( ( $n - 48 ) / 12 ) );
+}
+
+my $sample_rate = 44100;
+
+for my $key ( 0 .. 87 ) {
+    my $freq = key2freq( $key );
+    my $bandwidth = 2 * ( $freq - key2freq( $key - 0.5 ) );
+    my $N = POSIX::ceil( $sample_rate / $bandwidth );
+    my $k = POSIX::ceil( $freq / $bandwidth );
+
+    my $new_freq = $sample_rate * ( $k / $N );
+
+    printf "%d\t%f\t%f\t%d\t%f\t%f\n",
+        $key,
+        $freq,
+        $bandwidth,
+        $N,
+        $new_freq,
+        $new_freq - $freq,
+    ;
+}
