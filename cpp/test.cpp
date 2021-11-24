859fd66fb0353fa222fa8909a5b90bd212bfcd46
Stanislaw Pusep <creaktive@gmail.com>
2021-11-24 19:55:09 +0100

    TAP FTW!!!!111

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -3,17 +3,27 @@
 
 using namespace std;
 
+// kill me
+unsigned static TEST_COUNT = 0;
+#define TEST_OK(expression, description) { \
+  if (expression) {\
+    cout << "ok " << ++TEST_COUNT << " - " << description << endl; \
+  } else { \
+    cout << "not ok " << ++TEST_COUNT << " - " << description << endl; \
+    cerr << __FUNCTION__ << " failed on line " << __LINE__ << endl; \
+  } \
+}
+
 void testRingBuffer() {
   RingBuffer *rb = new RingBuffer(15);
-  assert(rb != NULL);
+  TEST_OK(rb != NULL, "object is not null");
 
-  // is it initialized to zero?
-  assert(rb->read(0) == 0); 
+  TEST_OK(rb->read(0) == 1, "initalized to zeroes"); 
 
   // write one single element
   rb->write(1);
-  assert(rb->read(0) == 1);
-  assert(rb->read(1) == 0);
+  TEST_OK(rb->read(0) == 1, "insertion succeeded");
+  TEST_OK(rb->read(1) == 0, "boundary shifted");
 
   // write 9 more elements
   for (unsigned i = 2; i < 10; i++)
@@ -21,23 +31,24 @@ void testRingBuffer() {
 
   // read in sequence
   for (unsigned i = 0; i < 10; i++)
-    assert(rb->read(9 - i) == i);
+    TEST_OK(rb->read(9 - i) == i, "sequence value matches");
 
   // overflow
   for (unsigned i = 10; i < 20; i++)
     rb->write(i);
 
   // both the head and the tail are expected values
-  assert(rb->read(0) == 19);
-  assert(rb->read(15) == 4);
+  TEST_OK(rb->read(0) == 19, "head as expected");
+  TEST_OK(rb->read(15) == 4, "tail as expected");
 
   // reading beyond the capacity of ring buffer wraps it around
-  assert(rb->read(16) == 19);
-  assert(rb->read(17) == 18);
+  TEST_OK(rb->read(16) == 19, "wrap back to 0");
+  TEST_OK(rb->read(17) == 18, "wrap back to 1");
 }
 
 int main(int argc, char *argv[]) {
   testRingBuffer();
 
+  cout << "1.." << TEST_COUNT << endl;
   return 0;
 }
