2323e7cde440bfa479254a95eefa388b3e37ccec
Stanislaw Pusep <creaktive@gmail.com>
2022-02-22 08:17:09 +0100

    saner defaults

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -13,24 +13,24 @@ using namespace std;
 void help();
 void help() {
   cout << "Usage:" << endl;
-  cout << "\tsox -d -traw -b32 -c1 -efloat - | ./pianolizer [options]" << endl;
+  cout << "\tsox -V -d -traw -r44100 -b32 -c1 -efloat - | ./pianolizer | sudo misc/hex2ws281x.py" << endl;
   cout << endl;
   cout << "Options:" << endl;
   cout << "\t-h\tthis" << endl;
-  cout << "\t-b\tbuffer size; default: 735 (1/60th of a second for 44.1kHz sample rate)" << endl;
+  cout << "\t-b\tbuffer size; default: 256" << endl;
   cout << "\t-s\tsample rate; default: 44100 (Hz)" << endl;
   cout << "\t-p\tA4 reference frequency; default: 440 (Hz)" << endl;
-  cout << "\t-a\taverage window (effectively a low-pass filter for the output); default: 0.050 (seconds; 0 to disable)" << endl;
+  cout << "\t-a\taverage window (effectively a low-pass filter for the output); default: 0.1 (seconds; 0 to disable)" << endl;
   cout << endl;
   cout << "Description:" << endl;
   exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[]) {
-  size_t bufferSize = 735; // aim for 60 fps
+  size_t bufferSize = 256; // known to work on RPi3b
   int sampleRate = 44100;
   float pitchFork = 440.;
-  float averageWindow = 0.050;
+  float averageWindow = 0.1;
 
   for (;;) {
     switch (getopt(argc, argv, "b:s:p:a:h")) {
