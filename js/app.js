4b82e206c992ab56aef9bd24eaabb9087430271e
Stanislaw Pusep <creaktive@gmail.com>
2022-09-04 14:55:58 +0200

    Split HTML/CSS/JS

---
+++ js/app.js
@@ -0,0 +1,272 @@
+import { PianoKeyboard, Spectrogram, Palette } from './visualization.js'
+
+let audioContext, audioSource, microphoneSource, pianolizer
+let levels, palette
+
+fetch('palette.json')
+  .then(response => response.json())
+  .then(data => { palette = new Palette(data) })
+
+const audioElement = document.getElementById('input')
+const playToggle = document.getElementById('play-toggle')
+const playRestart = document.getElementById('play-restart')
+const sourceSelect = document.getElementById('source')
+const pianolizerUI = document.getElementById('pianolizer')
+
+const pianoKeyboard = new PianoKeyboard(document.getElementById('keyboard'))
+pianoKeyboard.drawKeyboard()
+const spectrogram = new Spectrogram(
+  document.getElementById('spectrogram'),
+  pianoKeyboard.keySlices,
+  600
+)
+
+const midi = new Float32Array(pianoKeyboard.keysNum)
+if (navigator.requestMIDIAccess) {
+  navigator.requestMIDIAccess()
+    .then(
+      midiAccess => {
+        // connecting MIDI to input function
+        for (const input of midiAccess.inputs.values()) {
+          input.onmidimessage = message => {
+            const firstNoteIndex = 36
+            const [command, note, velocity] = message.data
+            switch (command) {
+              case 0x90:
+                midi[note - firstNoteIndex] = velocity / 0x7f
+                break
+              case 0x80:
+                midi[note - firstNoteIndex] = 0
+                break
+            }
+          }
+        }
+
+        sourceSelect.add(new Option('MIDI input only', '#'))
+      })
+}
+
+function draw (currentTimestamp) {
+  if (
+    (playToggle.disabled || !audioElement.paused) &&
+    levels !== undefined &&
+    palette !== undefined
+  ) {
+    const audioColors = palette.getKeyColors(levels)
+    const midiColors = palette.getKeyColors(midi)
+    pianoKeyboard.update(audioColors, midiColors)
+    spectrogram.update(audioColors, midiColors)
+  }
+  window.requestAnimationFrame(draw)
+}
+
+function isPureJS () {
+  return window.location.search.toLowerCase() === '?purejs'
+}
+
+async function setupAudio () {
+  if (audioContext === undefined) {
+    audioContext = new (window.AudioContext || window.webkitAudioContext)()
+
+    // Thanks for nothing, Firefox!
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1572644
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1636121
+    // https://github.com/WebAudio/web-audio-api-v2/issues/109#issuecomment-756634198
+    const fetchText = url => fetch(url).then(response => response.text())
+    const pianolizerImplementation = isPureJS()
+      ? 'js/pianolizer.js'
+      : 'js/pianolizer-wasm.js'
+    const modules = await Promise.all([
+      fetchText(pianolizerImplementation),
+      fetchText('js/pianolizer-worklet.js')
+    ])
+    const blob = new Blob(modules, { type: 'application/javascript' })
+    await audioContext.audioWorklet.addModule(URL.createObjectURL(blob))
+
+    pianolizer = new AudioWorkletNode(audioContext, 'pianolizer-worklet')
+    pianolizer.port.onmessage = event => {
+      // TODO: use SharedArrayBuffer for syncing levels
+      levels = event.data
+    }
+
+    audioSource = audioContext.createMediaElementSource(audioElement)
+    audioSource.connect(pianolizer)
+  }
+
+  audioSource.connect(audioContext.destination)
+  pianolizer.parameters.get('smooth').value = parseFloat(document.getElementById('smoothing').value)
+  pianolizer.parameters.get('threshold').value = parseFloat(document.getElementById('threshold').value)
+}
+
+async function setupMicrophone (deviceId) {
+  await setupAudio()
+  audioSource.disconnect(audioContext.destination)
+
+  if (microphoneSource === undefined) {
+    await navigator.mediaDevices.getUserMedia({ audio: { deviceId }, video: false })
+      .then(stream => {
+        microphoneSource = audioContext.createMediaStreamSource(stream)
+        microphoneSource.connect(pianolizer)
+
+        // for whatever reason, once selected, the input can't be switched
+        const selectedLabel = '*' + deviceId
+        for (const option of sourceSelect.options) {
+          if (option.value.charAt(0) === '*' && option.value !== selectedLabel) {
+            option.disabled = true
+          }
+        }
+      })
+      .catch(error => window.alert('Audio input access denied: ' + error))
+  } else {
+    microphoneSource.connect(pianolizer)
+  }
+}
+
+async function loadLocalFile () {
+  const fileHandles = await window.showOpenFilePicker({
+    types: [
+      {
+        description: 'Audio',
+        accept: {
+          'audio/*': ['.mp3', '.flac', '.ogg', '.wav']
+        }
+      }
+    ],
+    excludeAcceptAllOption: true,
+    multiple: false
+  })
+  const fileData = await fileHandles[0].getFile()
+  audioElement.src = URL.createObjectURL(fileData)
+}
+
+sourceSelect.onchange = event => {
+  audioElement.pause()
+  playToggle.innerText = 'Play'
+
+  const selectedValue = event.target.value
+  if (selectedValue.charAt(0) === '*') {
+    // microphone source
+    playToggle.disabled = true
+    playRestart.disabled = true
+    setupMicrophone(selectedValue.substring(1))
+  } else {
+    // <audio> element source
+    playToggle.disabled = false
+    playRestart.disabled = false
+    try { microphoneSource.disconnect(pianolizer) } catch { console.warn('Microphone was not connected') }
+    audioElement.style['pointer-events'] = 'auto'
+    if (selectedValue === '/') {
+      // only Chrome & Opera can do this at the time of writing
+      loadLocalFile()
+    } else if (selectedValue === '#') {
+      // "MIDI solo" mode
+      playToggle.disabled = true
+      playRestart.disabled = true
+      levels = new Float32Array(pianoKeyboard.keysNum)
+    } else {
+      audioElement.src = `${selectedValue}?_=${Date.now()}` // never cache
+    }
+  }
+}
+
+playToggle.onclick = async event => {
+  if (audioElement.paused) {
+    await setupAudio()
+    audioElement.play()
+    playToggle.innerText = 'Pause'
+  } else {
+    audioElement.pause()
+    playToggle.innerText = 'Play'
+  }
+}
+
+playRestart.onclick = event => {
+  audioElement.load()
+  playToggle.innerText = 'Play'
+}
+
+document.getElementById('rotation').oninput = event => {
+  if (palette !== undefined) {
+    palette.rotation = event.target.value
+  }
+}
+
+document.getElementById('smoothing').oninput = event => {
+  const value = parseFloat(event.target.value)
+  document.getElementById('smoothing-value').innerText = value.toFixed(3)
+  if (pianolizer !== undefined) {
+    pianolizer.parameters.get('smooth').value = value
+  }
+}
+
+document.getElementById('threshold').oninput = event => {
+  const value = parseFloat(event.target.value)
+  document.getElementById('threshold-value').innerText = value.toFixed(3)
+  if (pianolizer !== undefined) {
+    pianolizer.parameters.get('threshold').value = value
+  }
+}
+
+const myURL = window.location.href.replace(/\?.*/, '')
+const implementationWASM = document.getElementById('implementation-wasm')
+implementationWASM.onclick = () => {
+  window.location.href = myURL
+}
+const implementationPureJS = document.getElementById('implementation-purejs')
+implementationPureJS.onclick = () => {
+  window.location.href = myURL + '?purejs'
+}
+if (isPureJS()) {
+  implementationPureJS.checked = true
+} else {
+  implementationWASM.checked = true
+}
+
+if (window.showOpenFilePicker !== undefined) {
+  sourceSelect.add(
+    new Option('Load local audio file', '/'),
+    0
+  )
+}
+
+pianolizerUI.ondragover = event => {
+  event.preventDefault()
+}
+pianolizerUI.ondrop = event => {
+  event.preventDefault()
+  if (event.dataTransfer.items) {
+    for (const item of event.dataTransfer.items) {
+      if (item.kind === 'file' && item.type.match('^audio/(flac|mpeg|ogg|x-wav)$')) {
+        audioElement.pause()
+        playToggle.innerText = 'Play'
+
+        const fileData = item.getAsFile()
+        audioElement.src = URL.createObjectURL(fileData)
+
+        sourceSelect.value = '?'
+        document.getElementById('drop-label').innerText = fileData.name
+        break
+      }
+    }
+    event.dataTransfer.items.clear()
+  } else {
+    console.warn('DataTransferItemList interface unavailable')
+  }
+}
+
+navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+  .then(() => {
+    navigator.mediaDevices.enumerateDevices()
+      .then(devices => {
+        devices.filter(device => device.kind === 'audioinput')
+          .reverse()
+          .forEach(device => {
+            sourceSelect.add(
+              new Option('Input: ' + device.label, '*' + device.deviceId),
+              0
+            )
+          })
+      })
+  })
+
+window.requestAnimationFrame(draw)
