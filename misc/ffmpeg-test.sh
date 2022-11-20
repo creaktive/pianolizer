d6f30f9729c29ca122bdf9a3cf10dbb92c0ed4a8
Stanislaw Pusep <creaktive@gmail.com>
2022-11-20 13:28:50 +0100

    parametrize ffmpeg-test.sh

--- misc/ffmpeg-test.sh
+++ misc/ffmpeg-test.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-INPUT=../audio/chromatic.mp3
-RESAMPLE=8000
-BUFFER_SIZE=2000
-THRESHOLD=0.05
+BUFFER_SIZE=${BUFFER:-512}
+FILTERS=${FILTERS:-"asubcut=27,asupercut=20000"}
+INPUT=${INPUT:-"../audio/chromatic.mp3"}
+KEYS=${KEYS:-88}
+REFERENCE=${REFERENCE:-48}
+RESAMPLE=${RESAMPLE:-46520}
+SMOOTHING=${SMOOTHING:-0.04}
+THRESHOLD=${THRESHOLD:-0.05}
 exec \
-    ffmpeg -loglevel quiet -i $INPUT -ar $RESAMPLE -ac 1 -f f32le -c:a pcm_f32le - \
-    | ../pianolizer -b $BUFFER_SIZE -s $RESAMPLE -t $THRESHOLD
+    ffmpeg -loglevel quiet -i $INPUT -ac 1 -af $FILTERS -ar $RESAMPLE -f f32le -c:a pcm_f32le - \
+    | ../pianolizer -a $SMOOTHING -b $BUFFER_SIZE -k $KEYS -r $REFERENCE -s $RESAMPLE -t $THRESHOLD
