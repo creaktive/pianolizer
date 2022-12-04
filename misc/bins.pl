04753982abbc2eefee978ea4079a6fa8d14777ef
Stanislaw Pusep <creaktive@gmail.com>
2022-12-04 14:58:50 +0100

    Perl fixes

--- misc/bins.pl
+++ misc/bins.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/env perl
 use 5.036;
 
-use Getopt::Long ();
-use List::Util ();
-use POSIX ();
+use Getopt::Long qw(GetOptions);
+use List::Util qw(max);
+use POSIX qw(floor);
 
-sub key2freq ( $n, $s ) {
+sub key2freq($n, $s) {
     state $r = { 61 => 33, 88 => 48 };
-    my $o = $n - ( $r->{$s} || die "don't know manuals with $s keys\n" );
-    return 440 * ( 2 ** ( $o / 12 ) );
+    my $o = $n - ($r->{$s} || die "don't know manuals with $s keys\n");
+    return 440 * (2 ** ($o / 12));
 }
 
-Getopt::Long::GetOptions(
+GetOptions(
     'sample_rate=i'     => \my $sample_rate,
     'keyboard_size=i'   => \my $keyboard_size,
     'tolerance=f'       => \my $tolerance,
@@ -21,27 +21,27 @@ $keyboard_size ||= 61;
 $tolerance ||= 1;
 
 my $highest_error = 0;
-for my $key ( 0 .. $keyboard_size - 1 ) {
-    my $old_freq = key2freq( $key, $keyboard_size );
-    my $bandwidth = 2 * ( key2freq( $key + 0.5 * $tolerance, $keyboard_size ) - $old_freq );
-    my $N = POSIX::floor( $sample_rate / $bandwidth );
-    my $k = POSIX::floor( $old_freq / $bandwidth );
+for my $key (0 .. $keyboard_size - 1) {
+    my $old_freq = key2freq($key, $keyboard_size);
+    my $bandwidth = 2 * (key2freq($key + 0.5 * $tolerance, $keyboard_size) - $old_freq);
+    my $N = floor($sample_rate / $bandwidth);
+    my $k = floor($old_freq / $bandwidth);
 
-    my $delta = abs( $sample_rate * ( $k / $N ) - $old_freq );
-    for ( my $i = $N - 1;; $i--) {
-        my $tmp_delta = abs( $sample_rate * ( $k / $i ) - $old_freq );
-        if ( $tmp_delta < $delta ) {
+    my $delta = abs($sample_rate * ($k / $N) - $old_freq);
+    for (my $i = $N - 1;; $i--) {
+        my $tmp_delta = abs($sample_rate * ($k / $i) - $old_freq);
+        if ($tmp_delta < $delta) {
             $delta = $tmp_delta;
             $N = $i;
         } else {
             last;
         }
     }
-    my $new_freq = $sample_rate * ( $k / $N );
+    my $new_freq = $sample_rate * ($k / $N);
     my $new_bandwidth = $sample_rate / $N;
 
-    my $error = 100 * ( $new_freq - $old_freq ) / $bandwidth;
-    $highest_error = List::Util::max( $highest_error, abs( $error ) );
+    my $error = 100 * ($new_freq - $old_freq) / $bandwidth;
+    $highest_error = max $highest_error, abs($error);
     printf "%d\t%f\t%f\t%f\t%f\t%f\t%d/%d\n",
         $key,
         $old_freq,
--- misc/spectrum.pl
+++ misc/spectrum.pl
@@ -1,25 +1,22 @@
 #!/usr/bin/env perl
-use 5.020;
-use experimental qw( signatures );
-use warnings qw( all );
-no warnings qw( experimental::signatures );
+use 5.036;
 
-use Cpanel::JSON::XS ();
+use JSON::PP ();
 use POSIX ();
 
 # use constant PI => 3.1415926535897932384626433832795;
 #
-# sub rainbow ($p) {
+# sub rainbow($p) {
 #     return [
-#         POSIX::floor( 128 + 127 * sin( PI + $p ) ),
-#         POSIX::floor( 128 + 127 * sin( PI + $p + 2 * PI / 3 ) ),
-#         POSIX::floor( 128 + 127 * sin( PI + $p + 4 * PI / 3 ) ),
+#         POSIX::floor(128 + 127 * sin(PI + $p)),
+#         POSIX::floor(128 + 127 * sin(PI + $p + 2 * PI / 3)),
+#         POSIX::floor(128 + 127 * sin(PI + $p + 4 * PI / 3)),
 #     ];
 # }
 #
 # my @colors;
-# for (my $i = 0; $i < 2 * PI; $i += ( PI / 6 ) ) {
-#    unshift @colors => rainbow( $i );
+# for (my $i = 0; $i < 2 * PI; $i += (PI / 6)) {
+#    unshift @colors => rainbow($i);
 # }
 # push @colors => shift @colors;
 # push @colors => shift @colors;
@@ -39,13 +36,13 @@ my @colors = (
     [0xff, 0x00, 0x80], # rose
 );
 
-say Cpanel::JSON::XS::encode_json( \@colors );
+say JSON::PP::encode_json(\@colors);
 exit;
 
 my $scale = 64;
 my @rainbow;
-for my $c ( @colors ) {
-    for ( 1 .. $scale ) {
+for my $c (@colors) {
+    for (1 .. $scale) {
         push @rainbow =>
             sprintf '%3d %3d %3d', @$c
     }
@@ -57,5 +54,5 @@ P3
 255
 END_PPM
 
-say join( "\t", @rainbow )
-    for 1 .. $scale
+say join("\t", @rainbow)
+    for 1 .. $scale;
