cddd6a5a41d1dbb3d3257caaebfd12e39794f68e
Stanislaw Pusep <creaktive@gmail.com>
2023-04-03 14:57:10 +0200

    parametrize reference pitch, too

--- misc/bins.pl
+++ misc/bins.pl
@@ -5,26 +5,29 @@ use Getopt::Long qw(GetOptions);
 use List::Util qw(max);
 use POSIX qw(floor);
 
-sub key2freq($n, $s, $o = 0) {
+sub key2freq($n, $s, $p, $o) {
     state $r = { 61 => 33, 88 => 48 };
     $o ||= $r->{$s} || die "don't know manuals with $s keys; specify --offset\n";
-    return 440 * (2 ** (($n - $o) / 12));
+    return $p * (2 ** (($n - $o) / 12));
 }
 
+my $sample_rate = 44100;
+my $keyboard_size = 61;
+my $tolerance = 1.0;
+my $offset;
+my $pitch = 440.0;
 GetOptions(
-    'sample_rate=i'     => \my $sample_rate,
-    'keyboard_size=i'   => \my $keyboard_size,
-    'tolerance=f'       => \my $tolerance,
-    'offset=i'          => \my $offset,
+    'sample_rate=i'     => \$sample_rate,
+    'keyboard_size=i'   => \$keyboard_size,
+    'tolerance=f'       => \$tolerance,
+    'offset=i'          => \$offset,
+    'pitch=f'           => \$pitch,
 );
-$sample_rate ||= 44100;
-$keyboard_size ||= 61;
-$tolerance ||= 1;
 
 my $highest_error = 0;
 for my $key (0 .. $keyboard_size - 1) {
-    my $old_freq = key2freq($key, $keyboard_size, $offset);
-    my $bandwidth = 2 * (key2freq($key + 0.5 * $tolerance, $keyboard_size, $offset) - $old_freq);
+    my $old_freq = key2freq($key, $keyboard_size, $pitch, $offset);
+    my $bandwidth = 2 * (key2freq($key + 0.5 * $tolerance, $keyboard_size, $pitch, $offset) - $old_freq);
     my $N = floor($sample_rate / $bandwidth);
     my $k = floor($old_freq / $bandwidth);
 
