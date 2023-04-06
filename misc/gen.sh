f229f274524e47b23ff58da8a1dd65459404016e
Stanislaw Pusep <creaktive@gmail.com>
2023-04-06 19:23:23 +0200

    square/sawtooth examples with more overtones

--- misc/gen.sh
+++ misc/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sox -r 44100 -n saw.wav synth 3 saw 440
+sox -r 44100 -n saw.wav synth 3 saw 110
 sox -r 44100 -n sin.wav synth 3 sin 440
-sox -r 44100 -n squ.wav synth 3 squ 440
+sox -r 44100 -n squ.wav synth 3 squ 110
 sox -r 44100 -n noise.wav synth 3 pink
